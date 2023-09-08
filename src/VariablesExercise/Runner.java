1f28471e8bb67ff8e0afa12785879bb47212353d
Kaz-UK <karen.atkinson.uk@gmail.com>
2023-09-08 12:39:37 +0100

    Add variable exercise

---
+++ src/VariablesExercise/Runner.java
@@ -0,0 +1,21 @@
+package VariablesExercise;
+
+public class Runner {
+    public static void main(String[] args) {
+    //    String firstname = "Karen";
+    //    String lastname = "Atkinson";
+    //    String name = firstname + " " + lastname;
+    //    System.out.println(name);
+
+        float num1 = 12;
+        float num2 = 8;
+        System.out.println(num1 + num2);
+        float result = num1 / num2;
+        System.out.println(result);
+        int num3 = 8;
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3);
+
+    }
+}
